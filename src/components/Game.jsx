07d77a15c07f5a865b9b1f147171b5e310e288e7
claudiasrn <perezserranoclaudia@gmail.com>
2026-08-17 15:06:55 +0200

    feat: shuffle curated artwork order into roundIDs state

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,21 +1,20 @@
 import Card from "./Card.jsx";
 import "./Game.css";
+import shuffle from "../utils/shuffle.js";
+import { useState } from "react";
+
+const curatedObjectIDs = [
+	436528, 436530, 436524, 436535, 436534, 436533, 437138, 438008, 437107,
+	437112, 437115, 438004,
+];
 
 export default function Game() {
-	const curatedObjectIDs = [
-		436528, 436530, 436524, 436535, 436534, 436533, 437138, 438008, 437107,
-		437112, 437115, 438004,
-	];
+	const [roundIDs, setRoundIDs] = useState(() => shuffle(curatedObjectIDs));
 
 	return (
 		<div className="game">
-			{curatedObjectIDs.map((id) => {
-				return (
-					<Card
-						key={id}
-						id={id}
-					/>
-				);
+			{roundIDs.map((id) => {
+				return <Card key={id} id={id} />;
 			})}
 		</div>
 	);
